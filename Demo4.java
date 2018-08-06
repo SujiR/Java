62f3e5121c2d73215c1454db6207165b20307e28
SujiR <41675788+SujiR@users.noreply.github.com>
2018-08-06 22:49:26 +0530

    package access

---
+++ Demo4.java
@@ -0,0 +1,11 @@
+package CntnuMypack;
+
+import  MyPack.Demo1;
+public class Demo4 extends Demo1 
+{
+public Demo4()
+{
+	z=30;
+	v=40;
+}
+}
---
+++ Demo5.java
@@ -0,0 +1,15 @@
+package CntnuMypack;
+
+import MyPack.Demo1;
+
+public class Demo5
+{
+public static void main(String args[])
+{
+	Demo1 obj=new Demo1();
+	public Demo5()
+	{
+		objDemo1.v=40;
+	}
+}
+}
