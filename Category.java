71160a64ca52ec366213caa2d69b4d5c286a2a42
SujiR <41675788+SujiR@users.noreply.github.com>
2018-12-15 21:30:02 +0530

    model

--- Category.java
+++ Category.java
@@ -6,33 +6,39 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 
-
 @Entity
 @Table
 public class Category 
 {
-	@Id
-	@GeneratedValue
-public int categoryId;
-private String categoryName;
-private String categoryDesc;
-public int getCategoryId() {
+  @Id 
+  @GeneratedValue
+  public int categoryId;
+  private  String categoryName;
+  private String categoryDesc;
+
+  public int getCategoryId() 
+  {
 	return categoryId;
 }
-public void setCategoryId(int categoryId) {
+public void setCategoryId(int categoryId)
+{
 	this.categoryId = categoryId;
 }
-public String getCategoryName() {
+public String getCategoryName() 
+{
 	return categoryName;
 }
-public void setCategoryName(String categoryName) {
+public void setCategoryName(String categoryName) 
+{
 	this.categoryName = categoryName;
 }
-public String getCategoryDesc() {
+public String getCategoryDesc() 
+{
 	return categoryDesc;
 }
-public void setCategoryDesc(String categoryDesc) {
+public void setCategoryDesc(String categoryDesc) 
+{
 	this.categoryDesc = categoryDesc;
 }
-
+  
 }
--- Supplier.java
+++ Supplier.java
@@ -9,27 +9,34 @@ import javax.persistence.Table;
 @Table
 public class Supplier
 {
- @Id
- @GeneratedValue
- private int supplierId;
- private String supplierName;
- private String supplierLocation;
-public int getSupplierId() {
+	@Id
+	@GeneratedValue
+public int supplierId;
+private String supplierName;
+private String supplierLocation;
+public int getSupplierId()
+{
 	return supplierId;
 }
-public void setSupplierId(int supplierId) {
+public void setSupplierId(int supplierId) 
+{
 	this.supplierId = supplierId;
 }
-public String getSupplierName() {
+public String getSupplierName() 
+{
 	return supplierName;
 }
-public void setSupplierName(String supplierName) {
+public void setSupplierName(String supplierName) 
+{
 	this.supplierName = supplierName;
 }
-public String getSupplierLocation() {
+public String getSupplierLocation() 
+{
 	return supplierLocation;
 }
-public void setSupplierLocation(String supplierLocation) {
+public void setSupplierLocation(String supplierLocation) 
+{
 	this.supplierLocation = supplierLocation;
 }
+
 }
